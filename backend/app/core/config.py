c4e69dffd9225f7e68e0ec3f960f5b4cef114fc7
Mohammad Imrul Hasan <mail@imrul.net>
2025-04-01 12:44:57 +0600

    config added

---
+++ backend/app/core/config.py
@@ -0,0 +1,10 @@
+from pydantic import BaseSettings
+
+class Settings(BaseSettings):
+    DATABASE_URL: str = "sqlite:///./social_auto.db"
+    SECRET_KEY: str = "your_secret_here"
+
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
