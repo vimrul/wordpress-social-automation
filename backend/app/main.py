b97722cfe2aa2e73d14705a30e02d5e2c3a32492
Mohammad Imrul Hasan <mail@imrul.net>
2025-04-01 13:32:24 +0600

    Frontend updated

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,7 +11,7 @@ app = FastAPI(
 # Enable CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:5173"],  # explicitly allow your frontend
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
